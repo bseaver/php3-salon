3d7b1ef3b95bd88955837f1c5cca55bfe7bd8619
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-02-24 10:28:19 -0800

    Pass deleteAll, save and getAll tests (src/Stylist.php)

--- src/Stylist.php
+++ src/Stylist.php
@@ -44,17 +44,34 @@
 
         function save()
         {
+            $name = addslashes($this->getName());
+            $contact_info = addslashes($this->getContactInfo());
 
+            $GLOBALS['DB']->exec("INSERT INTO stylists
+                (name, contact_info) VALUES
+                ('$name', '$contact_info');"
+            );
+            $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
         {
-
+            $output = array();
+            $results = $GLOBALS['DB']->query("SELECT * FROM stylists ORDER BY name;");
+            foreach ($results as $result) {
+                $stylist = new Stylist(
+                    $result['name'],
+                    $result['contact_info'],
+                    $result['id']
+                );
+                array_push($output, $stylist);
+            }
+            return $output;
         }
 
         static function deleteAll()
         {
-
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
         }
 
         static function findById($id)
